ef4603abe93c7fb677a2b426a6e00f96686e2957
Parvez Mia <42247008+pz1971@users.noreply.github.com>
2022-07-16 20:30:22 +0600

    typing

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,69 @@
+// a text object
+var text
+// text splitting into characters will be stored in charList
+var charList 
+
 initialize = ()=>{
-    const text = texts[0]
+    text = texts[0]
     const textContainer = document.getElementById('text-container')
-    const ar = text.text.split('')
-    for(let i = 0; i < ar.length; i++){
+
+    // split into list of single characters
+    charList = text.text.split('')
+    
+    for(let i = 0; i < charList.length; i++){
         const span = document.createElement('span')
-        span.innerHTML = ar[i]
-        span.id = i
+        span.innerHTML = charList[i]
+        // initially all characters are untyped
+        span.className = 'untyped'
+        span.id = 'ch-' + i
+        // append this span to textContainer
         textContainer.appendChild(span)
     }
-    console.log(ar)
 }
 
-typing = (e) => {
+// currently typing position
+var pos = 0
+
+const typing = (e) => {
     e = e || window.event
-    var char = e.key || e.which
-    // console.log(char)
+    var key = e.key || e.which
+
+    // if already completed typing, do nothing
+    if(pos >= charList.length)
+        return
+    
+    if(key == charList[pos]){
+        // change class to correctly-typed
+        document.getElementById('ch-' + pos).className = 'correctly-typed'
+        pos++
+    }
+    else if(key == ' '){
+        // whitespace completes a word
+        // move pos to the begin of the next word
+        var pos2 = charList.indexOf(' ', pos) + 1
+        while(pos < pos2){
+            // change class to incorrectly-typed
+            document.getElementById('ch-' + pos).className = 'incorrectly-typed'
+            pos++
+        }
+    }
+    else{
+        // change class to incorrectly-typed
+        document.getElementById('ch-' + pos).className = 'incorrectly-typed'
+        pos++
+    }
+}
+
+const checkIfBackspace = (e) => {
+    e = e || window.event
+    var key = e.key || e.which
+
+    if(key === 'Backspace'){
+        // if pos is at beginning of a (word / text) or at the end of the text, do nothing
+        if(pos == 0 || pos == charList.length || charList[pos - 1] == ' ')
+            return
+        // change class to untyped
+        document.getElementById('ch-' + pos).className = 'untyped'
+        pos--
+    }
 }
