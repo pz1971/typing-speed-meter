d63b06a16f8d8cc7110e3c12e2935c3f5e6f9cb1
Parvez Mia <42247008+pz1971@users.noreply.github.com>
2022-07-18 19:30:11 +0600

    more data

--- Data/texts.js
+++ Data/texts.js
@@ -1,6 +1,51 @@
+// source: https://www.goodreads.com/quotes
 const texts = [
     {
         text: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
-        wordCount: 41
-    }
+        author: "Marilyn Monroe"
+    },
+    {
+        text: "Be yourself; everyone else is already taken.",
+        author: "Oscar Wilde"
+    },
+    {
+        text: `You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.`,
+        author: "William W. Purkey"
+    },
+    {
+        text: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        author: "Albert Einstein"
+    },
+    {
+        text: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+        author: "Bernard M. Baruch"
+    },
+    {
+        text: "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+        author: "Dr. Seuss"
+    },
+    {
+        text: "You only live once, but if you do it right, once is enough.",
+        author: "Mae West"
+    },
+    {
+        text: "Don’t walk in front of me… I may not follow Don’t walk behind me… I may not lead Walk beside me… just be my friend",
+        author: "Albert Camus"
+    },
+    {
+        text: "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+        author: "Maya Angelou"
+    },
+    {
+        text: "Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do.",
+        author: "Rob Siltanen"
+    },
+    {
+        text: "I believe that everything happens for a reason. People change so that you can learn to let go, things go wrong so that you appreciate them when they're right, you believe lies so you eventually learn to trust no one but yourself, and sometimes good things fall apart so better things can fall together.",
+        author: "Marilyn Monroe"
+    },
+    {
+        text: "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.",
+        author: "H. Jackson Brown Jr., P.S. I Love You"
+    },
 ];
